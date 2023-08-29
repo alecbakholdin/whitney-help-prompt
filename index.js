3a42a0fad9a07f924be8f66bc499bc24d32a217e
Alec Bakholdin <43560338+alecbakholdin@users.noreply.github.com>
2023-08-29 12:46:01 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 const readline = require('readline-sync');
 
-// your code here
+// below is an example of how to ask a user for information
+const name = readline.question('What is your name?');
+console.log("Hello " + name + "!");
+
+// the respones you get is a string. We need to convert that over to a number
+const ageStr = readline.question('How old are you?');
+const age = parseInt(ageStr);
+console.log("You are " + age + " years old");
