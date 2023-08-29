4222e84ed3deefb7a45093b6e16e6cdc26e51eac
Alec Bakholdin <alecbakholdin@gmail.com>
2023-08-29 16:51:05 +0000

    fixed bugs

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const readline = require('readline-sync');
+import readline from 'readline-sync';
 
 // below is an example of how to ask a user for information
 const name = readline.question('What is your name?');
@@ -7,4 +7,6 @@ console.log("Hello " + name + "!");
 // the respones you get is a string. We need to convert that over to a number
 const ageStr = readline.question('How old are you?');
 const age = parseInt(ageStr);
-console.log("You are " + age + " years old");
+console.log("You are going to be " + (age + 4) + " years old in 4 years");
+// notably, this doesn't work. Try it and see if you understand why
+// console.log("You are going to be " + ageStr + 4 + " years old in 4 years")
